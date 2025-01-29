a00b246bf14b913a257456563529ea51fca0da78
Jules Schoonman <jules.schoonman@gmail.com>
2025-01-29 21:40:29 +0100

    iiif output

--- src/constants.ts
+++ src/constants.ts
@@ -3,4 +3,5 @@ export const dlcsManifestBase = "https://dlc.services/iiif-manifest/v3/7/15/";
 export const dlcsImageBase = "https://dlc.services/iiif-img/v3/7/15/";
 export const manifestUriBase =
   "https://tu-delft-heritage.github.io/beeldbank-data/";
+// export const manifestUriBase = "http://127.0.0.1:5500/iiif/";
 export const homePageBase = "https://heritage.tudelft.nl/objects/";
